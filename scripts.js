125393a324255cd367a815fd3159d3a4e0cb724b
jess2d <jessicap_silveira@hotmail.com>
2019-10-14 14:18:33 +1300

    Add togglePlay function

--- scripts.js
+++ scripts.js
@@ -1,5 +1,20 @@
 //get our elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
 
 //build ou functions
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
 
 //hook up the event likstners 
+
+video.addEventListener('click', togglePlay);
